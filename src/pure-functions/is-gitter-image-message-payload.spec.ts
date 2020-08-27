9cf251d3ebff0522c5b7e630e90c0c407ab5137e
Huan LI (李卓桓) <zixia@zixia.net>
2020-08-27 23:35:17 +0800

    compatible markdown image

--- src/pure-functions/is-gitter-image-message-payload.spec.ts
+++ src/pure-functions/is-gitter-image-message-payload.spec.ts
@@ -6,11 +6,25 @@ import { GitterRoomMessagePayload } from '../gitter'
 
 import { isGitterImageMessagePayload } from './is-gitter-image-message-payload'
 
-test('isGitterImageMessagePayload', async (t) => {
+test('isGitterImageMessagePayload Gitter.im image', async (t) => {
   const URL_THUMB = 'https://files.gitter.im/5d007267d73408ce4fc3056f/7rkf/thumb/image.png'
   const URL       = 'https://files.gitter.im/5d007267d73408ce4fc3056f/7rkf/image.png'
 
   const TEXT = `[![image.png](${URL_THUMB})](${URL})`
+
+  const payload = {
+    text: TEXT,
+  } as GitterRoomMessagePayload
+
+  const ret = isGitterImageMessagePayload(payload)
+  t.true(ret, 'should identify image payload')
+  t.equal(ret, URL, 'should get image url')
+})
+
+test('isGitterImageMessagePayload: markdown image', async (t) => {
+  const URL = 'https://developers.google.com/assistant/interactivecanvas/images/interactivecanvasgame.gif'
+
+  const TEXT = `![gif.gif](${URL})`
 
   const payload = {
     text: TEXT,
--- src/pure-functions/is-gitter-image-message-payload.ts
+++ src/pure-functions/is-gitter-image-message-payload.ts
@@ -2,10 +2,15 @@ import { GitterRoomMessagePayload } from '../gitter'
 
 function isGitterImageMessagePayload (payload: GitterRoomMessagePayload): false | string {
   // text: '[![image.png](https://files.gitter.im/5d007267d73408ce4fc3056f/7rkf/thumb/image.png)](https://files.gitter.im/5d007267d73408ce4fc3056f/7rkf/image.png)',
-  // const RE = /^\[!\[[^]]+\]\(https:\/\/files\.gitter\.im\/[^)]+\)\]\(([^)]+)\)$/
-  const RE = /^\[!\[[^\]]+\]\(https:\/\/files\.gitter\.im\/[^)]+\)\]\(([^)]+)\)$/
+  const RE_GITTER = /^\[!\[[^\]]+\]\(https:\/\/files\.gitter\.im\/[^)]+\)\]\(([^)]+)\)$/
+  // text: '![gif.gif](https://domain.com/image.gif)'
+  const RE_MD = /^!\[[^\]]*\]\(([^)]+)\)$/
 
-  const matches = payload.text.match(RE)
+  let matches = payload.text.match(RE_GITTER)
+  if (!matches) {
+    matches = payload.text.match(RE_MD)
+  }
+
   if (matches) {
     return matches[1]
   }
